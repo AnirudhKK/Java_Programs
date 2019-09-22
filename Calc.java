6b9d68b514ca82dda8cf31ad50029756d8bef58d
AnirudhKK <49266081+AnirudhKK@users.noreply.github.com>
2019-09-22 22:25:44 +0530

    Added Calculator Class with functions
    A simple .java file of simple class with add(), sum(), multi(), div()

---
+++ Calc.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Calc
+{
+	int add(int a, int b)
+	{
+		return a+b;
+	}
+	int sub(int a, int b)
+	{
+		return a-b;
+	}
+	int multi(int a, int b)
+	{
+		return a*b;
+	}
+	double div(int a, int b)
+	{
+		double res=a/b;
+		return res;
+	}
+}
+	
